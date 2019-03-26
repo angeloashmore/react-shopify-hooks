84305663738668330feadd1966c2b251ca68fbf2
Angelo Ashmore <angelo.ashmore@walltowall.com>
2019-03-26 11:21:48 -1000

    refactor: remove unused imports

--- src/__tests__/hooks.js
+++ src/__tests__/hooks.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react'
+import React from 'react'
 import { renderHook, cleanup, act } from 'react-hooks-testing-library'
 
 import { createClient } from '../__testutils__/createClient'
